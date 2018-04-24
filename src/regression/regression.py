37731297c85c88a8ac6c3e7c7269f8228c630088
francisng0201 <cng10@illinois.edu>
2018-04-23 19:27:29 -0500

    add question

--- src/regression/regression.py
+++ src/regression/regression.py
@@ -2,10 +2,11 @@ import pandas
 import math
 import numpy as np
 import sqlite3 as sql
+import os.path as path
 
 EWMA_SEED = 0.1
 PROBABILITY_FIRST_PROBLEM_CORRECT = 0.894
-
+PATH = path.dirname(path.abspath(path.join(__file__ ,"../..")))
 
 class AccuracyModel(object):
 	def __init__(self, total_done, correct, correct_array):
@@ -66,7 +67,7 @@ class AccuracyModel(object):
 		return 1.0 / (1.0 + math.exp(-z))
 
 def get_accuracy(student_id):
-	con = sql.connect("../../mydb.db")
+	con = sql.connect(PATH + "/mydb.db")
 	cur = con.cursor()
 	cur.execute("SELECT DISTINCT(SKILL) FROM STUDENT_TEST")
 	rows = cur.fetchall()
@@ -83,7 +84,19 @@ def get_accuracy(student_id):
 	con.close()
 	return d
 
-def next_question(d):
+def next_question(d, student_id):
+	#smaller_50 = {k: v for k, v in d.iteritems() if v < 0.5}
+	con = sql.connect(PATH + "/mydb.db")
+	cur = con.cursor()
+	questions = {}
+	#cur.execute("SELECT problemId FROM STUDENT_TEST WHERE ITEST_id = ?", (student_id,))
+	#row = cur.fetchall()
+	for k, v in d.iteritems():
+		cur.execute("SELECT body from Problem_Text WHERE Problem_Text.problem_id IN (SELECT problemId from STUDENT_TEST WHERE ITEST_id = ? AND SKILL = ?)", (student_id, k))
+		row = cur.fetchall()
+		questions[k] = row
+	#cur.execute("Select body from Problem_Text where ITEST_id = ? AND SKILL = ? order by actionId", (student_id, skill))
+	return questions
 
 def main():
 	# l = [
@@ -119,8 +132,10 @@ def main():
 	# accuracy = AccuracyModel(len(array), correct, array)
 	# print accuracy.predict()
 	#for i in ['1821','1079','3601','954','1483','5347','8','7258','336','337','5638','6746','312','522','688']:
-	d = get_accuracy(1079)
-	print d
+	student_id = 6746
+	d = get_accuracy(student_id)
+	ok = next_question(d, student_id)
+	print ok
 
 if __name__ == "__main__":
 	main()
